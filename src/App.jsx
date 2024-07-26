42b680e4701681118b6f03f1f492a73bb7e732a7
devesh tripathi <deveshtrip10@gmail.com>
2024-07-26 21:31:51 +0530

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
-import { Button, ButtonGroup } from "@material-tailwind/react";
 import Cards from './Cards';
 
 const App = () => {
   return (
     <div>
     <Cards/>
-    <Button>button</Button>
     </div>
   )
 }
